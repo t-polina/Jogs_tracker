0606db20d08894df942bf96c12e70fcad1b7ebda
t-polina <tsy.polina@gmail.com>
2019-12-11 16:50:47 +0300

    add style for createPage

--- src/view/compoments/UpdateAndCreateJog/UpdateAndCreateJog.jsx
+++ src/view/compoments/UpdateAndCreateJog/UpdateAndCreateJog.jsx
@@ -1,39 +1,36 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {createJog, updateJog} from '../../../store/thunks'
+import {createJog, getUser, updateJog} from '../../../store/thunks'
 import * as selectors from '../../../store/selectors'
-
-
+import UpdateAndCreateJogStyle from './UpdateAndCreateJog.css'
 import { Formik } from 'formik';
-import Button from "@material-ui/core/Button";
+
 class UpdateAndCreate extends React.Component {
     constructor(props) {
         super(props);
         this.idJog =this.props.location.pathname;
+        if(this.props.user===undefined){
+            this.props.getUser('hello');
+        }
+    };
 
-    }
-    componentDidMount() {
-
-
-    }
+    handelCancel = () => {
+        this.props.history.push('/jog')
+    };
 
     handelSave = data => {
-        // console.log(data)
-        // data.date =new Date(data.date).getTime().toString();
-
-        // console.log(data)
         if(this.idJog.indexOf('update',1) === 1){
             data.user_id=this.props.user.id;
             data.jog_id= this.idJog.substr(12);
             this.props.updateJog(data);
-        } else
-        this.props.createJog(data);
+        } else this.props.createJog(data);
+        this.props.history.push('/jog')
     };
 
     render() {
-
         return(
-            <section>
+            <section className='updateForm'>
+                <button className='backButton' onClick={this.handelCancel}><img className='cancel' alr={'cancel'}/></button>
                 <Formik
                     initialValues={{
                         distance: 0,
@@ -44,10 +41,10 @@ class UpdateAndCreate extends React.Component {
                 >
                     {({ handleSubmit, handleChange }) => (
                         <form onSubmit={handleSubmit}>
-                            <input className="input" name="distance" onChange={handleChange} />
-                            <input className="input"  name="date"  onChange={handleChange} />
-                            <input className="input" name="time" onChange={handleChange} />
-                            <Button type="submit" fullWidth variant="contained" color="primary"> Save </Button>
+                            <div className='field first'><span className='fieldName'>Distance</span> <input className="input" name="distance" onChange={handleChange} /></div>
+                            <diV className='field'>   <span className = 'fieldName'>Time</span>  <input className="input"  name="time"  onChange={handleChange} /></diV>
+                            <div className='field'>  <span className = 'fieldName'>Date</span>  <input className="input" name="date" onChange={handleChange} /></div>
+                            <div className='buttonContainer'><button className="saveButton" type="submit"> Save </button></div>
                         </form>
                     )}
                 </Formik>
@@ -64,6 +61,7 @@ const mapStateToProps = (state) => {
 };
 const mapDispatchToProps = (dispatch) => {
     return {
+        getUser: () => dispatch(getUser()),
         createJog:(jog) => dispatch(createJog(jog)),
         updateJog: (jog) => dispatch(updateJog(jog))
     }
